18520a37c85a78c07c345695785d41218b85e753
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-10 00:47:42 +0600

    deposit section bug fixed

--- js/deposit.js
+++ js/deposit.js
@@ -1,25 +1,35 @@
 // DRY ---> Do Not Repeat Yourself
-document.getElementById("btn-deposit").addEventListener("click", function () {
+
+function setValue(locationId, value){
+  const setLocation=document.getElementById(locationId);
+  setLocation.innerText=value;
+}
+
+
+document.getElementById("btn-deposit").addEventListener("click", function (){
   /* 
     1. get the element by id
     2. get the value from the element
     3. convert string value to a number
     */
-  const newDepositAmount = getInputFieldValueById("deposit-field");
+  const newDepositAmount =document.getElementById("deposit-field").value;
   /* 
     1. get previous deposit total by id
 
     */
-  const previousDepositTotal = getTextElementValueById("deposit-total");
+  const previousDepositTotal = document.getElementById("deposit-total").innerText;
 
   // calculate new deposit total
-  const newDepositTotal = previousDepositTotal + newDepositAmount;
-  console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+  const newDepositTotal = parseFloat(previousDepositTotal) + parseFloat(newDepositAmount);
+  
   // set deposit total value
-  setTextElementValueById("deposit-total", newDepositTotal);
+  setValue("deposit-total", newDepositTotal);
 
   // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
-  setTextElementValueById("balance-total", newBalanceTotal);
-);
+  const previousBalanceTotal =document.getElementById("balance-total").innerText;
+
+  const newBalanceTotal = parseFloat(previousBalanceTotal) + parseFloat(newDepositAmount);
+  setValue("balance-total", newBalanceTotal);
+  
+});
+
